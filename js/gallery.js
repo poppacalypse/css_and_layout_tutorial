bfddaaecc5fca5c57473d3748f00c0f0da58d57d
Carl Poppa <poppacalypse@gmail.com>
2020-06-19 18:36:12 +0800

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,34 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery() {
-  alert("Hello from the gallery file!")
-  alert(document.querySelector("h1").textContent);
+  let thumbnails = document.querySelectorAll("#gallery-thumbs div img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      // let newImageAlt = thumbnail.alt;
+
+      mainImage.setAttribute("src", newImageSrc);
+      // mainImage.setAttribute("alt", newImageAlt);
+      mainImage.setAttribute("alt", thumbnail.alt); // alternative
+
+      // Change which image is current.
+      let currentClass = "current";
+
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
